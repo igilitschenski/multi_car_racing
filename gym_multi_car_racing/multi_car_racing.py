5b370b46e208acf5c0cfc8cab1723183dac67e6a
Mert Albaba <mertalbaba@gmail.com>
2021-10-11 20:46:58 -0700

    Update multi_car_racing.py

--- gym_multi_car_racing/multi_car_racing.py
+++ gym_multi_car_racing/multi_car_racing.py
@@ -148,8 +148,7 @@ class MultiCarRacing(gym.Env, EzPickle):
         EzPickle.__init__(self)
         self.seed()
         self.reward_weights = [-100, 5, -20, -5]
-        self.
-        ious_norm = 0
+        self.previous_norm = 0
         self.num_feats = 49
         self.num_obsv = 48
         self.num_agents = num_agents
