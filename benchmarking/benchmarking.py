9ab3383c4d4ac4d1b05d400067738754ca7d69b5
ParsaVahidi <pvahidi@usc.edu>
2021-10-16 11:41:03 -0700

    ddqn benchmark result

--- benchmarking/benchmarking.py
+++ benchmarking/benchmarking.py
@@ -55,7 +55,6 @@ if __name__ == "__main__":
     parser.add_argument(
         "--render",
         action='store_true',
-        default=False,
         help="Render episodes during test.",
     )
     args = parser.parse_args()
