d525b192610e4b570a3fdeb6c80602a2b5ef975f
trishajani <35816373+trishajani@users.noreply.github.com>
2021-11-18 22:50:53 -0800

    add tester class

--- algorithms/PPO/ppo_image.py
+++ algorithms/PPO/ppo_image.py
@@ -18,22 +18,26 @@ from torch.distributions import Categorical
 import gym_multi_car_racing
 import numpy as np
 import matplotlib.pyplot as plt
-#from matplotlib.ticker import MaxNLocator
+from matplotlib import animation
+sys.path.insert(0, '../../benchmarking')
+from model_tester import TesterAgent
+
+
 
 
 #Hyperparameters
 learning_rate = 0.0001 #0.0005
 gamma         = 0.98
 lmbda         = 0.95
-eps_clip      = 0.1
+eps_clip      = 0.05
 K_epoch       = 1 #5#3
 T_horizon     = 5 #100 #100 #20
 
 num_frames = 5
-beta = 0.01
+beta = 0.1
 SCALE = 10.0  # Track scale
 PLAYFIELD = 2000 / SCALE  # Game over boundary
-max_temp = 5.0
+max_temp = 1.0
 ACTIONS = [
         (-1, 1, 0.2), (0, 1, 0.2), (1, 1, 0.2),  # Action Space Structure
         (-1, 1, 0), (0, 1, 0), (1, 1, 0),  # (Steering Wheel, Gas, Break)
@@ -41,12 +45,12 @@ ACTIONS = [
         (-1, 0, 0), (0, 0, 0), (1, 0, 0)
         ]
 VELOCITY_REWARD_PROPORTIONAL = 1.0
-VELOCITY_REWARD_LOW = -5.0
-ANGLE_DIFF_REWARD = -1.0
-ON_GRASS_REWARD = -1.0
-BACKWARD_REWARD = 0.0
+VELOCITY_REWARD_LOW = -50.0
+ANGLE_DIFF_REWARD = -20.0
+ON_GRASS_REWARD = -5.0
+BACKWARD_REWARD = -1.0
 
-n_epi_train   = 800
+n_epi_train   = 500
 n_epi_test = 10
 
 def to_grayscale(img):
@@ -130,29 +134,11 @@ class PPO(nn.Module):
         v = self.value(linear1_out).squeeze(-1)
         return v
 
-    # def forward(self, x, t=1.0):
-    #     conv1_out = F.relu(self.conv1(x))
-
-    #     if torch.isnan(conv1_out).any():
-    #             print("conv1_out has a nan")
-
-    #     conv2_out = F.relu(self.conv2(conv1_out))
-
-    #     flattened = torch.flatten(conv2_out, start_dim=1)  # N x 9*9*32
-    #     linear1_out = self.linear1(flattened)
-
-    #     policy_output = self.policy(linear1_out)
-    #     value_output = self.value(linear1_out).squeeze(-1)
-
-    #     probs = F.softmax(policy_output / t, dim=-1)
-    #     log_probs = F.log_softmax(policy_output, dim=-1)
-    #     return probs, log_probs, value_output
-      
-
+   
 def train(self):
     env = gym.make("MultiCarRacing-v0", 
                     num_agents = 1, 
-                    verbose = 1,
+                    verbose = 0,
                     direction='CCW',
                     use_random_direction = True, 
                     backwards_flag = True, 
@@ -160,23 +146,31 @@ def train(self):
                     use_ego_color = False, 
                     setup_action_space_func = action_space_setup,
                     get_reward_func = get_reward,
-                   episode_end_func=episode_end,
-                   observation_type='frames')
+                    #episode_end_func=episode_end,
+                    observation_type='frames')
     
-    #model = PPO(num_frames)
-    model_path = "./state_dict_model.pt"
-    #model.load_state_dict(torch.load(model_path))
+    #model_path = "./state_dict_model.pt"
+    #model.load_state_dict(torch.load("./state_dict_model_final.pt"))
     
-    scores = []
+
+    print_interval = 1
+    save_interval = 10
+
+    scores = np.zeros(n_epi_train)
    
-    print_interval = 5
-    save_interval = 50
+    
+    avg_loss_episode = np.zeros(n_epi_train)
+    avg_entropy_loss_episode = np.zeros(n_epi_train)
+    avg_value_loss_episode = np.zeros(n_epi_train)
+    avg_policy_loss_episode = np.zeros(n_epi_train)
+
 
     for n_epi in range(n_epi_train):
+        print("The episode is", n_epi)
         done = False
         score = 0.0
-        negative_reward_counter = 0;
-        slow_velocity_counter = 0;
+        negative_reward_counter = 0
+        slow_velocity_counter = 0
         s = env.reset()
         # Initialize state with repeated first frame
         car_id = 0
@@ -189,20 +183,19 @@ def train(self):
         temperature = np.exp(-(n_epi - n_epi_train) / n_epi_train * np.log(max_temp))
         
         action_list = []
+
+
         while not done:
             s_lst, a_lst, r_lst, sprime_lst, prob_lst, done_lst = [], [], [],[], [], []
+            loss_lst, entropy_loss_lst, value_loss_lst, policy_loss_lst = [], [], [], []
             for t in range(T_horizon):
                 s = add_frame(s_prime, s)
-                #prob = model(s, temperature)[0]
                 prob = model.pi(s, t=temperature)
-                #prob = model.pi(torch.from_numpy(s).float(), t=temperature)
                 m = Categorical(prob)
                 a = m.sample().item()
-                #action = env.cont_action_space[a]
                 action_vec = np.array(ACTIONS[a])
-                s_prime, r, done, _ = env.step(action_vec)
+                s_prime, r, done, total_score = env.step(action_vec)
                 r = r[0]
-                #print(r)
                 r /= 100
                 s_prime = preprocess_state(s_prime, car_id)
 
@@ -212,15 +205,18 @@ def train(self):
                 prob_lst.append(prob[0,a].item())
                 done_mask = 0 if done else 1
                 done_lst.append([done_mask])
-                env.render()
-                
-               
                 action_list.append(a)
-                #s = add_frame(s_prime, s)
                 sprime_frames = add_frame(s_prime, sprime_frames)
                 sprime_lst.append(sprime_frames)
-                score += r
-                if done:
+                #score += r
+                env.render()
+
+                negative_reward_counter = negative_reward_counter + 1 if r < 0 else 0
+
+                if done or negative_reward_counter >= 25:
+                    if negative_reward_counter >= 25:
+                        print ("negative_reward_counter >= 25, inside")
+                    print("done early! breaking out")
                     break
 
             
@@ -270,93 +266,181 @@ def train(self):
             entropy_loss = -(pi * torch.log(pi + 1e-20)).sum(dim=1).mean()
             policy_loss = torch.min(surr1, surr2).mean()
             value_loss = F.smooth_l1_loss(self.v(s_batch), td_target.detach())
-            loss = -1.0 * policy_loss + \
-                0.5 * value_loss + \
+            loss = -2.0 * policy_loss + \
+                2.0 * value_loss + \
                 -beta* entropy_loss
-
-            #entropy = -torch.sum(pi * torch.log(pi + 1e-20), dim=1, keepdim=True)
-            #t2 = F.smooth_l1_loss(self.v(s) , td_target.detach())
-            #loss = -torch.min(surr1, surr2) + F.smooth_l1_loss(self.v(s) , td_target.detach()) - entropy
 
             self.optimizer.zero_grad()
             loss.mean().backward()
             self.optimizer.step()
+
+            loss_lst.append(loss.detach().numpy().item())
+            entropy_loss_lst.append(entropy_loss.detach().numpy().item())
+            value_loss_lst.append(value_loss.detach().numpy().item())
+            policy_loss_lst.append(policy_loss.detach().numpy().item())
+
+            if negative_reward_counter >= 75:
+                print ("negative_reward_counter >= 75. end episode!")
+                break
             
-            #model.train_net(temperature, s=s_lst, a=a_lst, r=r_lst, s_prime=sprime_lst, done_mask=done_lst, prob_a=prob_lst)
+        scores[n_epi] = total_score['total_score'][0]
 
-            if n_epi%save_interval == 0:
-                torch.save(model.state_dict(), model_path)
+        avg_loss_episode[n_epi] = np.mean(loss_lst)
+        avg_entropy_loss_episode[n_epi] = np.mean(entropy_loss_lst)
+        avg_value_loss_episode[n_epi] = np.mean(value_loss_lst)
+        avg_policy_loss_episode[n_epi] = np.mean(policy_loss_lst)
 
 
-        scores.append(score)
-        #print(action_list)
-
-        #if n_epi%save_interval == 0:
-            #torch.save(model.state_dict(), model_path)
+        if n_epi%save_interval == 0 and n_epi !=0:
+            print("saving model!")
+            filepath = "./PPO_results/state_dict_model_{}.pt".format(n_epi)
+            torch.save(model.state_dict(), filepath)
+            plot_losses(scores,avg_loss_episode, avg_entropy_loss_episode, avg_value_loss_episode,avg_policy_loss_episode, n_epi+1)
 
         if n_epi%print_interval == 0:
-            print("# of episode :{}, avg score : {:.1f}".format(n_epi, score))
-            print(action_list)
-            print(prob)
+            print("# of episode :{}, avg score : {:.1f}".format(n_epi, total_score['total_score'][0]))
+            #print(action_list)
+            #print(prob)
     
     env.close()
 
-    torch.save(model.state_dict(), model_path)
-    return scores, action_list
+    torch.save(model.state_dict(), "./PPO_results/state_dict_model_final.pt")
+    return scores, action_list, avg_entropy_loss_episode, avg_loss_episode, avg_value_loss_episode, avg_policy_loss_episode
 
 
-def test():
-    
-    env = gym.make("MultiCarRacing-v0", num_agents=1, direction='CCW',
-                   use_random_direction=True, backwards_flag=True, h_ratio=0.25,
-                   use_ego_color=False)
-    model = PPO()
-    # Load saved model
-    model_path = "./saved_models/PPO/state_dict_model.pt"
-    model.load_state_dict(torch.load(model_path))
-    model.eval()
-    
-    score = 0.0
-    print_interval = 1
-    scores = []
 
-    for n_epi in range(n_epi_test):
-        score = 0.0
+def plot_losses(scores,avg_loss_episode, avg_entropy_loss_episode, avg_value_loss_episode,avg_policy_loss_episode, epi_number  ):
+    plt.figure(0)
+    plt.plot(np.arange(epi_number), scores[0:epi_number])
+    plt.xlabel("Episodes")
+    plt.ylabel("Score")
+    plt.savefig('./PPO_results/scores_{}.png'.format(epi_number-1))
+
+    plt.figure(1)
+    plt.plot(np.arange(epi_number), avg_loss_episode[0:epi_number])
+    plt.xlabel("Episodes")
+    plt.ylabel("Avg. total loss")
+    plt.savefig('./PPO_results/total_loss_{}.png'.format(epi_number-1))
+
+    plt.figure(2)
+    plt.plot(np.arange(epi_number), avg_entropy_loss_episode[0:epi_number])
+    plt.xlabel("Episodes")
+    plt.ylabel("Avg. entropy loss")
+    plt.savefig('./PPO_results/entropy_{}.png'.format(epi_number-1))
+
+
+    plt.figure(3)
+    plt.plot(np.arange(epi_number), avg_value_loss_episode[0:epi_number])
+    plt.xlabel("Episodes")
+    plt.ylabel("Avg. value loss")
+    plt.savefig('./PPO_results/value_{}.png'.format(epi_number-1))
+
+    plt.figure(4)
+    plt.plot(np.arange(epi_number), avg_policy_loss_episode[0:epi_number])
+    plt.xlabel("Episodes")
+    plt.ylabel("Avg. policy loss")
+    plt.savefig('./PPO_results/policy_{}.png'.format(epi_number-1))
+
+    plt.close('all')
+
+
+def evaluate(self):
+    num_test_episodes = 10
+    env = gym.make("MultiCarRacing-v0", 
+                    num_agents = 1, 
+                    verbose = 0,
+                    direction='CCW',
+                    use_random_direction = True, 
+                    backwards_flag = True, 
+                    h_ratio = 0.25,
+                    use_ego_color = False, 
+                    setup_action_space_func = action_space_setup,
+                    episode_end_func=lambda x, y: False,
+                    observation_type='frames', 
+                    seed=42)
+    score_arr = []
+    for n_epi in range(num_test_episodes):
+        print('Episode {}/{}'.format(n_epi+1, num_test_episodes))
         done = False
-        env.reset()
-        s = env.get_feat(car_id=0)
-        action_list = []
+        s = env.reset()
+        s = preprocess_state(s, 0)
+        s = s.repeat(1, num_frames, 1, 1)
         while not done:
+            prob = model.pi(s, t=1.0)
+            m = Categorical(prob)
+            a = m.sample().item()
+            action_vec = np.array(ACTIONS[a])
+            s_prime, r, done, info = env.step(action_vec)
+            #score = info['total_score']
+            #score_arr.append(score)
+            s_prime = preprocess_state(s_prime, 0)
+            s = add_frame(s_prime, s)
             
-            for t in range(T_horizon):
-                prob = model.pi(torch.from_numpy(s).float())
-                m = Categorical(prob)
-                a = m.sample().item()
-                action = env.cont_action_space[a]
-                action_list.append(a)
-                _, r, done, _ = env.step(action)
-                s_prime = env.get_feat(car_id=0)
-                model.put_data((s, a, r, s_prime, prob[a].item(), done))
-                s = s_prime
-                score += r.item()
-                env.render()
-
-        scores.append(score)
-        print("# of episode :{}, avg score : {:.1f}".format(n_epi, score))
-        print(action_list)
-        #if n_epi%print_interval==0 and n_epi!=0:
-            #print("# of episode :{}, avg score : {:.1f}".format(n_epi, score/print_interval))
-            #score = 0.0
-        
+            env.render()
     env.close()
-    return scores, action_list
+
+     
+
+class PPOTesterAgent(TesterAgent):
+    def __init__(self,
+                 model_path='./saved_models/PPO/state_dict_model_220.pt',
+                 car_id=0,
+                 num_frames=5,
+                 actions=ACTIONS,
+                 **kwargs
+                 ):
+        super().__init__(**kwargs)
+        self.car_id = car_id
+        self.frame_buffer = None
+        self.num_frames = num_frames
+        self.actions = actions
+        self.agent = self._load_model(model_path)
+
+    def _load_model(self, model_path):
+        agent = PPO(self.num_frames)
+        agent.load_state_dict(torch.load(model_path))
+        agent.eval()
+        return agent
+
+    def _update_frame_buffer(self, new_frame):
+        if self.frame_buffer is None:
+            self.frame_buffer = new_frame.repeat(1, self.num_frames, 1, 1)
+        else:
+            self.frame_buffer = add_frame(new_frame, self.frame_buffer)
+
+    def state_to_action(self, s):
+        """
+        This function should take the most recent state and return the
+        action vector in the exact same form it is needed for the environment.
+        If you are using frame buffer see example in _update_frame_buffer
+        how to take care of that.
+        """
+        s = preprocess_state(s, self.car_id)
+        self._update_frame_buffer(s)
+        prob = self.agent.pi(self.frame_buffer)
+        a = Categorical(prob).sample().item()
+        action_vec = np.array(self.actions[a])
+        return action_vec
+
+    @staticmethod
+    def setup_action_space(env):
+        """
+        This should be the same action space setup function that you used for training.
+        Make sure that the actions set here are the same as the ones used to train the model.
+        """
+        env.cont_action_space = ACTIONS
+        env.action_space = gym.spaces.Discrete(len(env.cont_action_space))
+
+    @staticmethod
+    def get_observation_type():
+        """
+        Simply return 'frames' or 'features'
+        """
+        return 'frames'
+
 
 if __name__ == '__main__':
     model = PPO(num_frames)
-    scores, action_list = train(model)
-    #print(scores)
-    print(action_list)
-    plt.plot(np.arange(len(scores)), scores)
-    plt.xlabel("Episodes")
-    plt.ylabel("Score")
-    plt.show()
+    model.load_state_dict(torch.load('saved_models/PPO/state_dict_model_220.pt'))
+    evaluate(model)
+
